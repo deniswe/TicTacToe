27e6602301b73256ead97ca6b58cdea2e6039d37
denis <ge48heh@mytum.de>
2023-05-02 11:49:12 +0200

    upodate

--- src/App.js
+++ src/App.js
@@ -10,10 +10,6 @@ export default function Game() {
   const [playing, setplaying] = useState(true)
 
   //Stops the game and sets the message
-  const testgitpush = () => {
-    
-  }
-
   const checkwin = () => {
     const winnning = [[0,1,2], [3,4,5], [6,7,8], [0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
     if (values[0] !== "" && values[1] !== "" && values[2] !== "" && values[3] !== "" && values[4] !== "" && values[5] !== "" && values[6] !== "" && values[7] !== "" && values[8] !== ""){
